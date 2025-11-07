c2e95729b55e977c4aa0c784c0337b20613f9616
Jaison Staloch Junior | Desktop-Home <staloch.dev@gmail.com>
2025-11-06 23:57:10 -0300

    chore: configure database connection and application properties

--- src/main/java/com/shopping/shoppingsystem/ShoppingSystemApplication.java
+++ src/main/java/com/shopping/shoppingsystem/ShoppingSystemApplication.java
@@ -8,6 +8,10 @@ public class ShoppingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingSystemApplication.class, args);
+
+		// Signaling the start of the application in the console
+		System.out.println("System started successfully!");
+		System.out.println("Access: http://localhost:8080");
 	}
 
 }
